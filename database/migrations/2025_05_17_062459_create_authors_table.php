29f5bc12893dc2c16cfb107a16ac461c15f79dc4
fakhirula <fakhirula27@gmail.com>
2025-06-01 22:47:56 +0700

    first commit

--- database/migrations/2025_05_17_062459_create_authors_table.php
+++ database/migrations/2025_05_17_062459_create_authors_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('photo');
+            $table->text('bio');
             $table->timestamps();
         });
     }
